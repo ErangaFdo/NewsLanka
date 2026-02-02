8f4be6372cefe7a602e7b1bc191d4dcdf7b54ef8
ErangaFernando404 <erangafdo1994@gmail.com>
2026-02-02 11:50:17 +0530

    update

--- app/(userdashboard)/home.tsx
+++ app/(userdashboard)/home.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl font-bold text-center">Welcome to user dashboard home</Text>
+      <Text className="text-blue-500 px-4 py-2 bg-blue-100 rounded-lg" onPress={() => router.push('/login')}>
+        login
+      </Text>
     </View>
   )
 }
